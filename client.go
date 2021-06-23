23e04f285d579a100e7bce181f9cb7b36952d958
bleau <xichong.ling@mail.mcgill.ca>
2021-06-23 14:53:31 +0800

    client, allowing users to type in and receives messages on their own terminal.

---
+++ client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func sendMes(conn net.Conn){
+	var input string
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		data,_,_ := reader.ReadLine()
+		input = string(data)
+
+		if strings.Compare(strings.ToUpper(input), "EXIT") == 0 {
+			conn.Close()
+			break
+		}
+
+		_,err := conn.Write([]byte(input))
+		if err != nil {
+			conn.Close()
+			fmt.Println("Connection failure")
+			break
+		}
+	}
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	go sendMes(conn)
+	buffer := make([]byte,1024)
+	for {
+		_, err_3 := conn.Read(buffer)
+		if err_3 != nil {
+			conn.Close()
+		}
+		fmt.Println(string(buffer))
+	}
+
+}
+
